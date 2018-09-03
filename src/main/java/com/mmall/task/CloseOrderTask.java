3d2c84043527089b15ae11991c46f52b8a6903f8
王长勇 <996817807@qq.com>
2018-09-04 00:37:27 +0800

    logback配置

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -58,7 +58,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-    //@Scheduled(cron = "0 */1 * * * ?") //每分钟执行
+    @Scheduled(cron = "0 */1 * * * ?") //每分钟执行
     public void closeOrderTashV3(){
         log.info("关闭订单定时任务启动");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout","5000"));
@@ -90,7 +90,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-    @Scheduled(cron = "0 */1 * * * ?") //每分钟执行
+    //@Scheduled(cron = "0 */1 * * * ?") //每分钟执行
     public void closeOrderTashV4(){
         RLock lock = redissonManager.getRedisson().getLock(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         boolean getLock = false;
